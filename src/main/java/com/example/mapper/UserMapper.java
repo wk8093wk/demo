3b862b0270a0d48d91a68da231cdb67bfb426c6a
wk8093wk <2785132877@qq.com>
2019-07-31 16:48:28 +0800

    IDEAGIT测试2

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -4,8 +4,11 @@ package com.example.mapper;
 import com.example.pojo.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserMapper {
     @Insert("insert into user(name,age) values(#{name},#{age})")
     void addUser(User user);
